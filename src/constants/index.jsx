64ee56e8cfda105011643b64599ee2167e38755b
TiLourinho <lourinho.tiago@gmail.com>
2022-08-28 18:15:46 -0300

    feat: constants file

---
+++ src/constants/index.jsx
@@ -0,0 +1,4 @@
+export const UPPERLETTERS = 'ABCDEFGHIJKLMNOPQSRTUVWXYZ';
+export const LOWERLETTERS = 'abcdefghijklmnopqrstuvwxyz';
+export const NUMBERS = '0123456789';
+export const SYMBOLS = '~!@#$%^&*()_+=|';
