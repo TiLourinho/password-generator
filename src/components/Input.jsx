9c198a20c72589d85332abb8059b1334de2516b4
TiLourinho <lourinho.tiago@gmail.com>
2022-08-28 18:17:28 -0300

    refactor: Input value and onChange issues

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Input = ({ label, title, type, id, value=10, min, max }) => {
+const Input = ({ label, title, type, id, value, min, max, onChange}) => {
   return (
     <div className="form-control">
       <label htmlFor={ label }>{ title }</label>
-      <input type={ type } id={ id } defaultValue={ value } min={ min } max={ max } />
+      <input type={ type } id={ id } defaultValue={ value } min={ min } max={ max } onChange={ onChange } />
     </div>
   );
 }
